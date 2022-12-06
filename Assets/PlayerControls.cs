f0e2b85fce483fb86e549ea7c89dbde3177f1807
krimelz <krimzida21@gmail.com>
2022-12-06 20:08:19 +0300

    Added new folders

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControls : MonoBehaviour
+{
+    [SerializeField]
+    private InputActionProperty input;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
