22a5eff3af79fb7c03b2aaf35a24da819ea30771
Krimelz <krimzida21@gmail.com>
2022-12-14 00:16:44 +0300

    Some changes

--- Assets/Codebase/Installers/PlayerInstaller.cs
+++ Assets/Codebase/Installers/PlayerInstaller.cs
@@ -3,7 +3,13 @@ using Zenject;
 
 public class PlayerInstaller : MonoInstaller
 {
+    [SerializeField]
+    private GameObject playerPrefab;
+    [SerializeField]
+    private Transform spawnPoint;
+
     public override void InstallBindings()
     {
+        Container.InstantiatePrefab(playerPrefab, spawnPoint.position, Quaternion.identity, null);
     }
 }
