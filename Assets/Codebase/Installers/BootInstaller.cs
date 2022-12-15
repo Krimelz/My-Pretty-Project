8d8fcc3e4c11272ffa48f2f1970cc3200f3295f9
krimelz <krimzida21@gmail.com>
2022-12-15 20:08:42 +0300

    Some changes

--- Assets/Codebase/Installers/BootInstaller.cs
+++ Assets/Codebase/Installers/BootInstaller.cs
@@ -4,19 +4,24 @@ using Zenject;
 
 namespace Codebase.Installers
 {
-    public class BootInstaller : MonoInstaller
-    {
-        [SerializeField] 
-        private InputService inputServicePrefab;
+	public class BootInstaller : MonoInstaller
+	{
+		[SerializeField]
+		private InputService inputServicePrefab;
 
-        public override void InstallBindings()
-        {
-            Container
-                .Bind<IInputService>()
-                .To<InputService>()
-                .FromComponentInNewPrefab(inputServicePrefab)
-                .AsSingle()
-                .NonLazy();
-        }
-    }
+		public override void InstallBindings()
+		{
+			BindInputService();
+		}
+
+		private void BindInputService()
+		{
+			Container
+				.Bind<IInputService>()
+				.To<InputService>()
+				.FromComponentInNewPrefab(inputServicePrefab)
+				.AsSingle()
+				.NonLazy();
+		}
+	}
 }
